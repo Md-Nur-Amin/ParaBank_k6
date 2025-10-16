c111d9f74e9e8d891840e1584201a3221286205f
Nur-Amin2 <rabbiii049@gmail.com>
2025-10-16 22:20:34 +0600

    post method - login & register

---
+++ paraBankPost_test.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+// export let options ={
+//     vus: 10,
+//     duration: '10s',
+//     iteration: 2,
+// }
+
+export default function(){
+    // let res = http.post('');
+    const payload = JSON.stringify({
+        email:'new@new.com',
+        password: 'new123',
+    });
+
+    const params = {
+        headers : {
+            'content-type': 'application/json',
+        },
+    };
+
+    const res = hhtp.post('https://reqres.in/api/login', payload, params);
+    check(res, {
+        'Login status is 200': (r)=> r.status === 200,
+        'Token recieved': (r)=> r.json('token') !== '',
+    })
+
+    sleep(1);
+
+}
